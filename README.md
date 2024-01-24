47efb54baf10e2c8774d29c3c8404d715c73545f
Murtaza Saifuddin <mortuze@gmail.com>
2024-01-24 01:07:50 -0500

    added desktop view, about screen

--- README.md
+++ README.md
@@ -1,30 +1,11 @@
-# React + TypeScript + Vite
+# Profile Page
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Finally getting stated on a portfolio project. This will be mostly me learning about frontend and implementing some dumb designs
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Stack
+- React
+- Tailwind CSS
+- Versel for deployment 
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+[Live](https://p01-profile.vercel.app/)
---
+++ src/Components/base.tsx
@@ -0,0 +1,131 @@
+import React from "react";
+
+export default function Base() {
+  function Divider() {
+    return (
+      <>
+        <br />
+        <p>---</p>
+        <br />
+      </>
+    );
+  }
+
+  function Date(props: { date: string }) {
+    return <span className="animate-pulse">{props.date}</span>;
+  }
+
+  function Links(props: { text: string; link: string }) {
+    return (
+      <span className=" break-words">
+        [
+        <span className="text-links">
+          <a href={props.link} target="_blank" rel="noopener noreferrer">
+            {props.text}
+          </a>
+        </span>
+        ]({props.link})
+      </span>
+    );
+  }
+
+  const variant = " ml-8 list-disc list-outside ";
+
+  return (
+    <>
+      <div className="font-main text-xl text-white">
+        <h1 className=" text-6xl">
+          # <span className="underline">About</span>
+        </h1>
+        <br />
+        <h2 className=" text-4xl ">
+          ## <span className="font-extrabold">Murtaza Saifuddin</span>
+        </h2>
+        <p className="text-xl">
+          I dont know, I am good at some stuff and bad at a lot others.
+        </p>
+        <ul className={variant}>
+          <li>Coder</li>
+          <li>Curious</li>
+        </ul>
+        <ul className={variant}>
+          <li>
+            <span className="underline">Horriable</span> at spelling
+          </li>
+          <li>
+            Procrastinate, Though of since: <Date date="18 Jan 24" />
+          </li>
+        </ul>
+        <br />
+        <h2 className=" text-4xl">## Links</h2>
+        <ul className=" ml-8 list-disc list-outside">
+          <li>
+            <Links text="Github" link="https://github.com/saifuddm" />
+          </li>
+          <li>
+            <Links
+              text="LinkedIn"
+              link="https://www.linkedin.com/in/saifuddm"
+            />
+          </li>
+        </ul>
+        <Divider />
+        <h1 className=" text-6xl"># Projects</h1>
+        <br />
+        <h2 className=" text-4xl">## Coming Soon ...</h2>
+        <br />
+        <h2 className=" text-4xl ">
+          ##{" "}
+          <Links text="Soco" link="https://github.com/SocialContracts/menu" />
+        </h2>
+        <ul className={variant}>
+          <li>React</li>
+          <li>Firebase</li>
+          <li>JS</li>
+        </ul>
+        <Divider />
+        <h1 className=" text-6xl"># Intrests</h1>
+        <br />
+        <h2 className=" text-4xl">## Coded/Designed</h2>
+        <ul className={variant}>
+          <li>
+            Last Engaged: <Date date="19 Jan 24" />
+          </li>
+        </ul>
+        <br />
+        <h2 className=" text-4xl">## Watching Tech YT</h2>
+        <ul className={variant}>
+          <li>
+            Last Engaged: <Date date="18 Jan 24" />
+          </li>
+        </ul>
+        <br />
+        <h2 className=" text-4xl">## Cooking</h2>
+        <ul className={variant}>
+          <li>
+            Last Engaged: <Date date="19 Jan 24" />
+          </li>
+        </ul>
+        <Divider />
+        <h1 className=" text-6xl"># Relax</h1>
+        <br />
+        <Links
+          text="Lofi Chill"
+          link="https://www.youtube.com/watch?v=rMBPCPE9_Xc"
+        />
+        <br />
+        <br />
+        <br />
+        <p>
+          * Sorry when I get better at responsive design this will be better.
+        </p>
+        <br />
+        <input
+          type="text"
+          placeholder="HAVE A NICE DAY"
+          className="w-full animate-pulse bg-transparent text-links"
+        />
+      </div>
+    </>
+  );
+}
